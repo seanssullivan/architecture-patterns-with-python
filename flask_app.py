d850f70466c0284089dad66400a43562cc2f92e9
Sean Sullivan <dev@seanmedia.ca>
2020-12-19 15:53:56 -0700

    Add service layer function

--- flask_app.py
+++ flask_app.py
@@ -16,13 +16,15 @@ app = Flask(__name__)
 @app.route("/allocate", methods=['POST'])
 def allocate_endpoint():
     session = get_session()
-    batches = repository.SqlAlchemyRepository(session).list()
+    repo = repository.SqlAlchemyRepository(session)
     line = model.OrderLine(
       request.json['orderid'],
       request.json['sku'],
       request.json['qty'],
     )
-
-    batchref = model.allocate(line, batches)
+    try:
+        batchref = services.allocate(line, repo, session)
+    except (model.OutOfStock, services.InvalidSku) as e:
+        return jsonify({'message': str(e)}), 400
 
     return jsonify({'batchref': batchref}), 201
